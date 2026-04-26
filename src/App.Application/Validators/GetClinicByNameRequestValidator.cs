e695fdcb57f7e0ced6cbd39cf07e0b9225af7df4
norhanmarei <mustafanour751@gmail.com>
2026-04-27 00:56:30 +0300

    Add validator for GetClinicByNameAsync

--- src/App.Application/Validators/GetClinicByNameRequestValidator.cs
+++ src/App.Application/Validators/GetClinicByNameRequestValidator.cs
@@ -3,21 +3,11 @@ using FluentValidation;
 namespace App.Application.Validators;
 public class GetClinicByNameRequestValidator : AbstractValidator<GetClinicByNameRequest>
 {
-  private bool BeValidText(string text)
-  {
-    foreach(char c in text)
-    {
-      if (char.IsControl(c))
-        return false;
-    }
-    return true;
-  }
   public GetClinicByNameRequestValidator()
   {
      RuleFor(x => x.Name)
        .Cascade(CascadeMode.Stop)
        .NotEmpty().WithMessage("Name must not be empty")
-       .MaximumLength(200).WithMessage("Name max length is 200")
-       .Must(BeValidText).WithMessage("Name must not contain invalid characters");
+       .MaximumLength(200).WithMessage("Name max length is 200");
   }
 }
